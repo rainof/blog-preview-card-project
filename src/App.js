a27d60f7a69bbfd906139f03e9205f62c2569525
rainof <13416303+rainof@users.noreply.github.com>
2024-07-27 15:03:57 +0700

    Update blog card styling and page background

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import blog_img from './assets/images/illustration-article.svg';
 function App() {
   return (
     <div className="card" style={{width: '18rem'}}>
-      <img className="card-img-top" src={blog_img} alt="blog" />
+      <img className="card-img" src={blog_img} alt="blog" />
       <div className="card-body">
         <h5 className="card-title">HTML & CSS foundations</h5>
         <p className="card-text">These languages are the backbone of every website, defining structure, content, and presentation.</p>
